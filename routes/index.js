04a095ad9b786f91340497477a0377385293d885
Tulsi Prasad <tulsi.prasad50@gmail.com>
2019-12-24 03:26:32 +0530

    :ok_hand: update index.js with moment pkg
    write datetime objects to be rendered using moment package

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,20 @@
-var express = require('express');
-var router = express.Router();
-var json = require('./data.json')
+const express = require('express');
+const router = express.Router();
+const moment = require("moment")
+const json = require('./data.json')
 
 // need 4 elements from the json array in blog.ejs file
-let data = []
+let ejsRenderedData = []
 
 for (let i=0; i<=3; i++) {
-  let release = json[i].release
 
-  let date = json[i].release.split("T")[0].replace(/-/g, '/')
+  let date = moment(json[i].release)
 
-  data.push({
+  ejsRenderedData.push({
     title: json[i].title,
     href: json[i].href,
     author: json[i].author,
-    release: date.split("/").reverse().join("/"),
+    release: date.format("DD/MM/YYYY"),
     cover: json[i].cover
   })
 }
@@ -22,7 +22,7 @@ for (let i=0; i<=3; i++) {
 /* GET home page. */
 router.get('/', function(req, res) {
   console.log("check")
-  res.render('index', {data: data});
+  res.render('index', { ejsRenderedData: ejsRenderedData });
 });
 
 
